21b71b2e766ca9a4e9edee8ba942e5f8001a019c
ezii <manu@korfmann.info>
2019-09-29 12:54:55 +0200

    …

---
+++ ruby-browser/browser.rb
@@ -0,0 +1,36 @@
+{
+   CLIENT_X: nil, # X in relation to the browsr including controls (all of theem)
+   CLIENT_Y: nil, #
+   CLIENT_DISPLAY_X: nil, # X in relation to the 
+   CLIENT_DISPLAY_Y: nil, #
+   CANVAS_X: nil, # x in relation to the wholle canvas (also hidden from screen)
+   CANVAS_Y: nil, #
+   SCREEN_X: nil, # x in relation to the visible screen
+   SCREEN_Y: nil
+}
+
+
+# or
+
+{
+   CLIENT_X: nil, # X in relation to the browsr including controls (all of theem)
+   CLIENT_Y: nil, #
+   CLIENT_DISPLAY_X: nil, # X in relation to the 
+   CLIENT_DISPLAY_Y: nil, #
+   CANVAS_X: nil, # x in relation to the wholle canvas (also hidden from screen)
+   CANVAS_Y: nil, #
+   SCREEN_X: nil, # x in relation to the visible screen
+   SCREEN_Y: nil
+}
+
+
+
+puts """
+
+	Way more introspectiv browser, show rulers to the developer about what client_x or screen_x are in this browser via the inspector.
+   
+       Show an image of a browser window with rulers showing CLIENT_X etc;
+
+        Show more information about the script code (Ruby).
+
+"""
